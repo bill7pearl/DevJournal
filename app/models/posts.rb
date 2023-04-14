bdc130355bc9a1799b00836d1c0e7701c3ff8c62
Billal <billal7chami@gmail.com>
2023-04-14 21:55:30 +0100

    Added validation for Post

--- app/models/posts.rb
+++ app/models/posts.rb
@@ -4,6 +4,16 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  after_save :update_posts_counter
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def five_latest_comments
+    comments.order('created_at DESC').limit(5)
+  end
+
   def update_posts_counter
     update_attribute(:posts_counter, posts.count)
   end
