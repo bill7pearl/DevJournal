ea6da6e2667be6f158ab0dcdd157bd8c30146a7f
Billal <billal7chami@gmail.com>
2023-04-14 02:38:41 +0100

    Fixed rubocop linters

--- app/models/posts.rb
+++ app/models/posts.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
   belongs_to :user
   has_many :likes
   has_many :comments
 
   def update_posts_counter
-    self.update_attribute(:posts_counter, self.posts.count)
+    update_attribute(:posts_counter, posts.count)
   end
 
   def five_recent_comments
