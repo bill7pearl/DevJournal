4d7a6b021803b7ecbb61d8ab3013a1ac66fb7459
Billal <billal7chami@gmail.com>
2023-04-15 10:27:16 +0100

    Fixed reviewer feedback by adding a test case to a custom method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_numericality_of(:posts_counter).only_integer.is_greater_than_or_equal_to(0) }
   end
+
+  describe 'three_recent_posts' do
+    let(:user) { create(:author) }
+    let!(:post1) { create(:post, author: user, created_at: 1.day.ago) }
+    let!(:post2) { create(:post, author: user, created_at: 2.days.ago) }
+    let!(:post3) { create(:post, author: user, created_at: 3.days.ago) }
+    let!(:post4) { create(:post, author: user, created_at: 4.days.ago) }
+  
+    it 'returns the three most recent posts' do
+      expect(user.three_recent_posts).to eq([post1, post2, post3])
+    end
+  
+    it 'does not include posts that are older than the three most recent' do
+      expect(user.three_recent_posts).not_to include(post4)
+    end
+  end  
 end
