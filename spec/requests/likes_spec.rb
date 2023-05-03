3685096973468b3e7300dd0f7ee9a4f2e87115f3
Salwa Ballouti <110976050+Salwa99@users.noreply.github.com>
2023-05-03 15:01:30 +0100

    Add tests for likes

---
+++ spec/requests/likes_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+RSpec.describe 'Likes', type: :request do
+  describe 'GET /new' do
+    before(:example) do
+      @user = User.create(name: 'Daniel',
+                          photo: 'https://picsum.photos/200/500',
+                          bio: 'El cashico', posts_counter: 4)
+      @post1 = Post.create(author: @user, title: 'Dudes', text: 'Theon Greyjoy', likes_counter: 0,
+                           comments_counter: 0)
+      @like1 = Like.create(author: @user, post: @post1)
+      @like2 = Like.create(author: @user, post: @post1)
+      @like3 = Like.create(author: @user, post: @post1)
+    end
+    it 'returns http success' do
+      get "/users/#{@user.id}/posts/#{@post1.id}"
+      expect(response).to have_http_status(200)
+    end
+  end
+end
